f822de9c248f848d8229714fd337e5120f62f6c5
ethangalva <82258388+ethangalva@users.noreply.github.com>
2022-12-14 12:41:03 -0800

    feat: +header responsive

---
+++ pages/components/header.jsx
@@ -0,0 +1,25 @@
+export default function Header() {
+    return (
+        <div className="h-14 w-screen flex px-6 absolute justify-between">
+            {/* BRAND */}
+            <div className="w-1/2 flex md:w-1/4 ">
+                <a className="flex items-center">
+                    <div className="text-xl font-bold md:text-2xl">RealEmpire</div>
+                </a>
+            </div>
+            {/* NAV MENU */}
+            <div className="hidden w-2/4 justify-start items-center gap-4 text-md font-semibold md:flex">
+                <a href="#home" className="">HOME</a>
+                <a href="about" className="">ABOUT</a>
+                <a href="#project" className="">PROJECT</a>
+                <a href="#contact" className="">CONTACT</a>
+            </div>
+            {/* SEND INQUIRY CTA */}
+            <div className="w-1/2 flex items-center justify-end text-md font-bold flex-nowrap md:text-2x md:w-1/4">
+                <a className="items-center" href="mailto:ethangalva@gmail.com">
+                    <div className="px-3 rounded-full bg-customGreen text-white hover:underline">SEND INQUIRY</div>
+                </a>
+            </div>
+        </div>
+    )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customGreen: '#4C604C',
+      }
+    },
   },
   plugins: [],
 }
